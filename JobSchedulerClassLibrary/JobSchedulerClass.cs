29d34054158a78202b2f6686c0cf7a4e9baa26db
M N Tushar <mntushar25@gmail.com>
2022-09-27 23:17:12 +0600

    complete job  task

--- JobSchedulerClassLibrary/JobSchedulerClass.cs
+++ JobSchedulerClassLibrary/JobSchedulerClass.cs
@@ -1,49 +1,115 @@
-﻿using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+﻿using CamcoMetrics.Service.IService;
+using CamcoMetrics.ViewModels.AutomatedAutomations;
+using CamcoMetrics.ViewModels.EmailQueue;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace JobSchedulerClassLibrary
+namespace CamcoMetrics.Library
 {
-    public class JobSchedulerClass : IHostedService, IDisposable
+    public class AutomationJobTask : BackgroundService
     {
-        private int executionCount = 0;
-        private readonly ILogger<JobSchedulerClass> _logger;
-        private Timer _timer;
+        private IServiceProvider _serviceProvider;
 
-        public JobSchedulerClass(ILogger<JobSchedulerClass> logger)
+        public AutomationJobTask([NotNull] IServiceProvider serviceProvider)
         {
-            _logger = logger;
+            _serviceProvider = serviceProvider;
         }
 
-        public Task StartAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _logger.LogInformation("Timed Hosted Service running.");
-
-            _timer = new Timer(DoWork, null, TimeSpan.Zero,
-                TimeSpan.FromSeconds(5));
-
-            return Task.CompletedTask;
+            await DueRecurringTask(stoppingToken);
         }
 
-        private void DoWork(object state)
+        public async Task DueRecurringTask(CancellationToken stoppingToken)
         {
-            var count = Interlocked.Increment(ref executionCount);
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                IAutomatedAutomationsService automatedAutomationsService = scope.ServiceProvider.GetRequiredService<IAutomatedAutomationsService>();
+                AutomatedAutomationsViewModel automation = automatedAutomationsService.GetAutomation(t => t.AutomationType == "Due Recurring Task");
 
-            _logger.LogInformation(
-                "Timed Hosted Service is working. Count: {Count}", count);
+                if (automation != null)
+                {
+                    if (automation.IsEnabled)
+                    {
+                        DateTime today = DateTime.Now;
+                        if (automation.LastRun.Date != today.Date || automation.NeedsRestart)
+                        {
+                            string dayName = today.DayOfWeek.ToString();
+                            if (automation.GetType().GetProperty(dayName).GetValue(automation).Equals(true) || automation.NeedsRestart)
+                            {
+                                if (automation.EarliestTime < today.TimeOfDay && today.TimeOfDay < automation.LatestTime || automation.NeedsRestart)
+                                {
+                                    await CheckPastDueTasks();
+                                    automation.LastRun = today;
+                                    automation.IsRunningNow = true;
+                                    await automatedAutomationsService.UpdateAsync(automation);
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        automation.IsRunningNow = false;
+                        await automatedAutomationsService.UpdateAsync(automation);
+                    }
+                }
+
+
+                await Task.Delay(automation.TimerTick, stoppingToken);
+
+                await DueRecurringTask(stoppingToken);
+            }
         }
 
-        public Task StopAsync(CancellationToken stoppingToken)
+        protected async Task CheckPastDueTasks()
         {
-            _logger.LogInformation("Timed Hosted Service is stopping.");
+            using (var scop = _serviceProvider.CreateScope())
+            {
+                ITasksService tasksService = scop.ServiceProvider.GetRequiredService<ITasksService>();
+                IEmployeeService employeeService = scop.ServiceProvider.GetRequiredService<IEmployeeService>();
+                IEmailService emailService = scop.ServiceProvider.GetRequiredService<IEmailService>();
 
-            _timer?.Change(Timeout.Infinite, 0);
+                var PastDueTasks = tasksService.GetRecurringTasksSync(x => x.UpcomingDate.HasValue && x.UpcomingDate < DateTime.Today);
+                foreach (var DueTask in PastDueTasks)
+                {
+                    if (!string.IsNullOrEmpty(DueTask.PersonResp))
+                    {
+                        //Here We Will Send Due Date Email
+                        string body = "<label style=\"font-weight:bold\"> INITIATOR: </label>" + DueTask.Initiator.ToUpper() +
+                            "<label style=\"font-weight:bold\"> DESCRIPTION: </label>" + DueTask.Description.ToUpper() +
+                            "<br><label style=\"font-weight:bold\"> UPCOMING DATE: </label>" + DueTask.UpcomingDate?.Date.ToString("MM/dd/yyyy") +
+                            "<br><label style=\"font-weight:bold\"> FREQUENCY: </label>" + DueTask.Frequency +
+                            "<br><label style=\"font-weight:bold\"> LINK TO MARK COMPLETED: </label>" + AppSettings.GetAppUrl() + "viewrecurringtasks/OpenTask/" + DueTask.Id.ToString();
 
-            return Task.CompletedTask;
+                        string Subject = "MISSED RECURRING TASK, " + DueTask.Description.ToUpper();
+
+                        var employee = await employeeService.GetEmployee(DueTask.PersonResp);
+
+                        if (string.IsNullOrEmpty(employee?.Email))
+                            continue;
+
+                        EmailQueueViewModel emailqueue = new()
+                        {
+                            Body = body,
+                            HasBeenSent = false,
+                            SendTo = employee?.Email,
+                            Subject = Subject
+                        };
+
+                        await emailService.InsertEmail(emailqueue);
+                    }
+                }
+            }
+
         }
 
-        public void Dispose()
+        public override async Task StopAsync(CancellationToken stoppingToken)
         {
-            _timer?.Dispose();
+            await base.StopAsync(stoppingToken);
         }
     }
 }
---
+++ JobSchedulerClassLibrary/ScopedProcessingService .cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace JobSchedulerClassLibrary
+{
+    public sealed class ScopedProcessingService : BackgroundService
+    {
+        private int executionCount = 0;
+        private readonly ILogger _logger;
+        private bool _action { get; set; } = false;
+
+        public ScopedProcessingService(ILogger<ScopedProcessingService> logger, bool action)
+        {
+            _logger = logger;
+            _action = action;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            if (_action)
+            {
+                CancellationToken st = new CancellationToken(true);
+                await DoWork(st);
+            }
+            else
+                await DoWork(stoppingToken);
+        }
+
+        public async Task DoWork(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                executionCount++;
+
+                _logger.LogInformation(
+                    "Scoped Processing Service is working. Count: {Count}", executionCount);
+
+                await Task.Delay(10000, stoppingToken);
+            }
+        }
+    }
+}
